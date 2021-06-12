e35e61bea28eac5b321782f494c3f484cb3d3c80
alter5 <36527069+alter5@users.noreply.github.com>
2021-06-12 16:29:31 -0400

    Fix crypto API return null error

--- public/data/cryptoAPI.mjs
+++ public/data/cryptoAPI.mjs
@@ -2,18 +2,22 @@
 // Common coin ids: bitcoin, ethereum, ...
 class cryptoAPI {
 
-    getValue(coinID, elementID) {
-        let value = ""
-        fetch("https://api.coingecko.com/api/v3/coins/", {
-                method: 'GET',
-                headers: {
-                    'id': coinID,
-                }
-            }).then(r => r.json()).then(data => {
-                // Get the current price of BTC
-                const value = data[0].market_data.current_price.usd.toFixed(2)
-                document.getElementById(elementID).innerHTML = value;
-            })
+    /**
+     * Get the current value of a cryptocurrency in USD
+     * @param coinID The id of the cryptocurrency from the CoinGecko API
+     */
+    async getValue(coinID) {
+        const response = await fetch("https://api.coingecko.com/api/v3/coins/", {
+            method: 'GET',
+            headers: {
+                'id': coinID,
+            }
+        }).then(r => r.json()).then(data => {
+            // Get the current price of the coin
+            return data[0].market_data.current_price.usd.toFixed(2)
+        })
+
+        return response
     }
 }
 
