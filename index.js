bce086b750aa0935237a7743a6e81dfb5cd21c3b
bartenfeldpa <bartenfeldpa@gmail.com>
2021-06-25 12:00:08 -0400

    Added more dynamic buttons on newstest

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ const requestOptions = {
   gzip: true
 };
 
+var CoinMarketCap = require("node-coinmarketcap");
+exports.index = function(req, res) {
+    // send moment to your ejs
+    res.render('index', { CoinMarketCap: CoinMarketCap });
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -24,7 +30,7 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/Karthik', (req, res) => res.render('pages/Karthik'))
   .get('/nedspage', (req, res) => res.render('pages/nedspage'))
-  .get('/al-bio', (req, res) => res.render('pages/al-bio'))
+  .get('/al-bio', (req, res) => res.render('pages/al-bio', {title:'Altin\'s Bio'}))
   .get('/paulspage', (req, res) => res.render('pages/paulspage'))
   .get('/newstest', (req, res) => res.render('pages/newstest'))
   .get('/serhiispage', (req, res) => res.render('pages/serhiispage'))
