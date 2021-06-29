d703559905f1cc97d82b0d64b4e12e52521ff744
alter5 <36527069+alter5@users.noreply.github.com>
2021-06-29 00:11:46 -0400

    fix(cryptoAPI): add ID to getCoin() return variable

--- public/data/cryptoAPI.js
+++ public/data/cryptoAPI.js
@@ -8,15 +8,17 @@ const base = `https://api.coingecko.com/api/v3/`
 async function getValue(coinID) {
     coinID = coinID.toLowerCase()
     const coin = await getCoin(coinID)
-    return coin[coinID].usd.toFixed(2)
+    return Number(coin.price).toFixed(2)
 }
 
 /**
  * Gets the current value, market cap, and 24hr change for a cryptocurrency
- * @param coinID The ID of a cryptocurrency from the CoinGecko API
+ * @param coinID The ID of a cryptocurrency to look up in the CoinGecko API
  */
 async function getCoin(coinID) {
-    return getCoins([coinID])
+    coinID = coinID.toLowerCase()
+    coin = (await getCoins([coinID]))[0]
+    return coin
 }
 
 /**
@@ -24,33 +26,25 @@ async function getCoin(coinID) {
  * @param coinIDs An array of cryptocurrency ids from the CoinGecko API
  */
 async function getCoins(coinIDs) {
-    const url = new URL(base + 'simple/price')
+    const response = await fetchCoins(coinIDs)
+    return formatCoins(response)
+}
+
+async function fetchCoins(coinIDs) {
+    const url = new URL(base + 'coins/markets')
     const params = {
-        'ids': coinIDs.join(),
-        'vs_currencies': 'usd',
-        'include_market_cap': true,
-        'include_24hr_change': true
+        'vs_currency': 'usd',
+        'price_change_percentage': '24h'
     }
+    // If an array of coin IDs has been provided, add it to the query
+    // If no coin IDs are specified, 
+    if (coinIDs) params["ids"] = coinIDs.join(",")
+
     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
 
     const response = await fetch(url).then(r => r.json())
         .then(data => {
-            /* Coins are returned in the following JSON format
-            {
-                "bitcoin": {
-                    "usd": 35674,
-                    "usd_market_cap": 668148892256.3638,
-                    "usd_24h_vol": 35636689529.785774,
-                    "usd_24h_change": -4.35081290184123
-                },
-                "ethereum": {
-                    "usd": 2380.23,
-                    "usd_market_cap": 276714108452.90765,
-                    "usd_24h_vol": 28220756437.30981,
-                    "usd_24h_change": 1.0382271224597475
-                }
-            } */
-            console.log("Retrieved specified coins", data)
+            console.log("Retrieved coins", data)
             return data
         })
     // Return the coins as a JSON object
@@ -59,27 +53,15 @@ async function getCoins(coinIDs) {
 
 async function getPopularCoins() {
     const response = await fetchPopularCoins()
-    return formatPopularCoins(response)
+    return formatCoins(response)
 }
 
 /**
  * Returns a list with 100 of the most popular cryptocurrencies, sorted by market cap in descending order
  */
 async function fetchPopularCoins() {
-    const url = new URL(base + 'coins/markets')
-    const params = {
-        'vs_currency': 'usd',
-        'price_change_percentage': '24h'
-    }
-
-    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
-
-    const response = await fetch(url).then(r => r.json())
-        .then(data => {
-            console.log("Retrieved popular coins", data)
-            return data
-        })
     // Return the coins as a JSON object
+    const response = await fetchCoins()
     return response
 }
 
@@ -87,11 +69,11 @@ async function fetchPopularCoins() {
  * Formats the response from the CoinGecko API for popular coins 
  * @param response The JSON object returned from the CoinGecko API
  */
-function formatPopularCoins(response) {
+function formatCoins(response) {
     let coins = []
     response.forEach(coin => {
         const name = coin.name
-        const tickerSymbol = coin["symbol"].toUpperCase()
+        const tickerSymbol = coin.symbol.toUpperCase()
         const price = coin.current_price.toFixed(2)
         const priceChange = coin.price_change_24h
 
@@ -101,6 +83,7 @@ function formatPopularCoins(response) {
             "price" : price,
             "priceChange" : priceChange
         })
+
     })
     return coins
 }
