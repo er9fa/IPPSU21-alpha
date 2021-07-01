775576e58ccba5fbd568b44a3c162a483abc7466
alter5 <36527069+alter5@users.noreply.github.com>
2021-07-01 01:10:31 -0400

    refactor(chartHelper): moved around functions and fixed minor bugs

--- public/helpers/chartHelper.js
+++ public/helpers/chartHelper.js
@@ -20,59 +20,11 @@ let setIntervalID_updateChart // The ID of the setInterval for updating the char
  * @returns A chart object (see Chart.js documentation)
  */
 function createChart(coinID, divElementID) {
-    chartXAxisTickers = getStartingXAxisTickers()
-    const data = {
-        labels: chartXAxisTickers,
-        datasets: [{
-            label: coinID + " value",
-            data: dataPoints,
-            backgroundColor: [
-                "rgb(63, 191, 127)"
-            ],
-            borderColor: [
-                "rgb(63, 191, 127)"
-            ],
-        }]
-    };
-    const config = {
-        type: 'line',
-        data,
-        options: {
-            responsive: false,
-            scales: {
-                y: {
-                    ticks: {
-                        // Changes the y-axis ticker values
-                        // Append a dollar sign to the tickers
-                        callback: function (value, index, values) {
-                            return '$' + value.toFixed(2);
-                        },
-                    },
-                    grace: 30
-                },
-                x: {
-                    grid: {
-                        drawOnChartArea: false,
-                    }
-                }
-            },
-            elements: {
-                point: {
-                    radius: 3
-                }
-            },
-            plugins: {
-                // Remove the legend displaying the meaning of the line on the chart
-                legend: {
-                    display: false
-                }
-            }
-        }
-    }
+    const chartConfig = getChartConfig()
 
     var chart = new Chart(
         document.getElementById(divElementID),
-        config
+        chartConfig
     );
 
     getCoin(coinID).then(gc_coin => {
@@ -84,6 +36,73 @@ function createChart(coinID, divElementID) {
     })
 
     return chart
+}
+
+/**
+ * Changes the coin being tracked by the chart. Meant to be used only with a dropdown menu's on select option listener.
+ * @param {Object} selectedOption The selected option object from the dropdown menu to be parsed (see dropdownHelper for info on what format the options must be in before binding)
+ */
+function changeChartCoin(selectedOption, arrayOfCoinsThatWereBindedToDropdown) {
+    console.log("next:", selectedOption)
+
+    const index = selectedOption.id
+    const pc_coin = arrayOfCoinsThatWereBindedToDropdown[index]
+
+    resetChart()
+
+    updateElementChartTitleAndCoinPrice(pc_coin.name, "chart-title", "chart-price", "price-change-image")
+
+    chart = createChart(pc_coin.id, "chart")
+}
+
+/**
+ * Updates the chart by adding the latest value of a coin as a datapoint on the line
+ */
+function updateChart() {
+    if (dataPoints.length > 8) {
+        incrementTimeByXSeconds(mostRecentTimeTicker, updateInterval)
+        chartXAxisTickers.push(convertTimeToString(mostRecentTimeTicker))
+        chart.update()
+    }
+    // Get most recent value of coin
+    getValue(chartCoin.id).then(value => {
+        // Add value to data set
+        dataPoints.push(value)
+        // Determine if price is higher or lower than the first recorded value
+        if (value < dataPoints[0]) {
+            // Change the chart's line color to red
+            chart.data.datasets[0].backgroundColor = "rgb(234, 67, 53)"
+            chart.data.datasets[0].borderColor = "rgb(234, 67, 53)"
+        } else {
+            // Change it to green
+            console.log("Status: ", chart.data)
+            chart.data.datasets[0].backgroundColor = "rgb(52, 168, 83)"
+            chart.data.datasets[0].borderColor = "rgb(52, 168, 83)"
+        }
+
+        // TODO: Get rid of these hard-coded ID's
+        // Updates the chart's HTML title and price <h> elements
+        updateElementChartTitleAndCoinPrice(chartCoin.name, "chart-title", "chart-price", "price-change-image")
+
+        // Chart.js method to refresh chart
+        chart.update()
+    })
+}
+
+/**
+ * After the chart has been created, it can be reset so that it tracks a new coin
+ */
+function resetChart() {
+    // Clears the chart library's data
+    chart.destroy()
+
+    // Clear the global variables storing chart datapoints and etc.
+    dataPoints = []
+    chartXAxisTickers = []
+    mostRecentTimeTicker = []
+
+    // Stop the chart updating interval
+    clearInterval(setIntervalID_updateChart) // TODO: Why is the chart updating with 2 datapoints at a time when selecting a new coin from the dropdown menu?
 }
 
 /**
@@ -133,57 +152,18 @@ function incrementTimeByXSeconds(time, seconds) {
 }
 
 /**
- * Updates the chart by adding the latest value of a coin as a datapoint on the line
- */
-function updateChart() {
-    if (dataPoints.length > 8) {
-        incrementTimeByXSeconds(mostRecentTimeTicker, updateInterval)
-        chartXAxisTickers.push(convertTimeToString(mostRecentTimeTicker))
-        chart.update()
-    }
-    getValue(chartCoin.id).then(value => {
-        dataPoints.push(value)
-        if (value < dataPoints[0]) {
-            chart.data.datasets[0].backgroundColor = "rgb(234, 67, 53)"
-            chart.data.datasets[0].borderColor = "rgb(234, 67, 53)"
-        } else {
-            chart.data.datasets[0].backgroundColor = "rgb(52, 168, 83)"
-            chart.data.datasets[0].borderColor = "rgb(52, 168, 83)"
-        }
-        
-        updateElementChartTitleAndCoinPrice(chartCoin.name, document.getElementById("chart-title"), document.getElementById("price-change-image"))
-
-        chart.update()
-    })
-}
-
-/**
- * Changes the coin being tracked by the chart. Meant to be used only with a dropdown menu's on select option listener.
- * @param {Object} coinOptionObject The selected option object from the dropdown menu to be parsed
- */
-function changeChartCoin(coinOptionObject) {
-    console.log("next:", coinOptionObject)
-
-    const index = coinOptionObject.id
-    const pc_coin = popularCoins[index]
-
-    resetChart()
-
-    updateElementChartTitleAndCoinPrice(pc_coin.name, document.getElementById("chart-title"), document.getElementById("price-change-image"))
-
-    chart = createChart(pc_coin.id, "chart")
-}
-
-/**
  * Updates the chart title elements. Use whenever the value of a coin has changed, or the coin being tracked on the chart is changed
  * @param {String} coinName The name of a coin to be displayed in the title
- * @param {String} titleTextElement The ID of a \<h\> element to bind the text to
- * @param {*} priceChangeImageElement The ID of a \<img\> element to bind the price change triangle image to
+ * @param {String} titleElementID The ID of a \<h\> element to bind the coin name to
+ * @param {String} priceChangeElementID The ID of a \<h\> element to bind the coin's price and percent change text to
+ * @param {String} priceChangeImageElementID The ID of a \<img\> element to bind the price change triangle image to
  */
-function updateElementChartTitleAndCoinPrice(coinName, titleTextElement, priceChangeImageElement) {
-    lastDatapoint = dataPoints[dataPoints.length - 1]
+function updateElementChartTitleAndCoinPrice(coinName, titleElementID, priceChangeElementID, priceChangeImageElementID) {
+    titleElement = document.getElementById(titleElementID)
+    priceChangeElement = document.getElementById(priceChangeElementID)
+    priceChangeImageElement = document.getElementById(priceChangeImageElementID)
 
-    let result = ""
+    lastDatapoint = dataPoints[dataPoints.length - 1]
     
     let percentChange
     if (dataPoints.length <= 1) {
@@ -194,29 +174,70 @@ function updateElementChartTitleAndCoinPrice(coinName, titleTextElement, priceCh
     percentChange = numeral(percentChange).format("0,0.000")
     lastDatapoint = numeral(lastDatapoint).format("0.00")
 
-    hasPriceIncreased = (percentChange >= 0)
+    const hasPriceIncreased = (percentChange >= 0)
     const priceChangeSymbolImage = (hasPriceIncreased) ?
         "/images/scrolling-bar-green-triangle.png" : "/images/scrolling-bar-red-triangle.png"
     const plusOrMinus = hasPriceIncreased ? "+" : ""
 
-    result = coinName + " $" + lastDatapoint + " (" + plusOrMinus + percentChange + "%)"
+    titleResult = coinName
+    priceChangeResult = "$" + lastDatapoint + " (" + plusOrMinus + percentChange + "%)"
+    priceChangeImageElementResult = priceChangeSymbolImage
 
-    titleTextElement.textContent = result
+    titleElement.textContent = titleResult
+    priceChangeElement.textContent = priceChangeResult
     priceChangeImageElement.src = priceChangeSymbolImage
 }
 
-/**
- * After the chart has been created, it can be reset so that it tracks a new coin
- */
-function resetChart() {
-    // Clears the chart library's data
-    chart.destroy()
-
-    // Clear the global variables storing chart datapoints and etc.
-    dataPoints = []
-    chartXAxisTickers = []
-    mostRecentTimeTicker = []
-
-    // Stop the chart updating interval
-    clearInterval(setIntervalID_updateChart) // TODO: Why is the chart updating with 2 datapoints at a time when selecting a new coin from the dropdown menu?
+function getChartConfig() {
+    chartXAxisTickers = getStartingXAxisTickers()
+    const data = {
+        labels: chartXAxisTickers,
+        datasets: [{
+            label: "Value",
+            data: dataPoints,
+            backgroundColor: [
+                "rgb(63, 191, 127)"
+            ],
+            borderColor: [
+                "rgb(63, 191, 127)"
+            ],
+        }]
+    };
+    const config = {
+        type: 'line',
+        data,
+        options: {
+            responsive: false,
+            scales: {
+                y: {
+                    ticks: {
+                        // Changes the y-axis ticker values
+                        // Append a dollar sign to the tickers
+                        callback: function (value, index, values) {
+                            return '$' + value.toFixed(2);
+                        },
+                    },
+                    grace: 30
+                },
+                x: {
+                    grid: {
+                        drawOnChartArea: false,
+                    }
+                }
+            },
+            elements: {
+                point: {
+                    radius: 3
+                }
+            },
+            plugins: {
+                // Remove the legend displaying the meaning of the line on the chart
+                legend: {
+                    display: false
+                }
+            }
+        }
+    }
+    return config
 }
+
