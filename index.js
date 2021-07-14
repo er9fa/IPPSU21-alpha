ddd0e7cb912057a9bdf32c5f2531dc89b671d589
Karthik E <karthikev13@gmail.com>
2021-07-13 22:57:14 -0400

    Solve Merge conflict

--- index.js
+++ index.js
@@ -59,6 +59,22 @@ express()
     });
   })
 
+  .get('/conversiondata', (req, res) => {
+   currency = req.query.amount;
+   dropdown1Symbol = req.query.symbol;
+   dropdown2Symbol = req.query.convert;
+
+
+    console.log(`https://pro-api.coinmarketcap.com/v1/tools/price-conversion?CMC_PRO_API_KEY=bc695b6b-f4b5-438b-9fab-88f108aead7f&amount=${currency}&symbol=${dropdown1Symbol}&convert=${dropdown2Symbol}`)
+    fetch(`https://pro-api.coinmarketcap.com/v1/tools/price-conversion?CMC_PRO_API_KEY=bc695b6b-f4b5-438b-9fab-88f108aead7f&amount=${currency}&symbol=${dropdown1Symbol}&convert=${dropdown2Symbol}`)
+    .catch(err => console.log(err))
+    .then(r => r.json()).then(data => {
+      console.log(data);
+      res.header('Access-Control-Allow-Origin', '*');
+      res.send(data);
+    });
+  })
+
   .get('/main', (req, res) => res.render('pages/main'))
   .get('/calculator', (req, res) => res.render('pages/calculator'))
   .get('/news', (req, res) => res.render('pages/news'))
